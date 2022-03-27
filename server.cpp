54ac53ff0e592993731fa5989551b3862b188987
Limerzzz <780039590@qq.com>
2022-03-27 16:04:07 +0800

    update .gitignore

--- server.cpp
+++ server.cpp
@@ -1,9 +1,11 @@
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <arpa/inet.h>
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
 #include <unistd.h>
+#include <fcntl.h>
 
 void errif(bool condition, const char* errmsg){
 	if(condition){
@@ -11,12 +13,27 @@ void errif(bool condition, const char* errmsg){
 		exit(1);	
 	}
 }
+int setnonblocking(int fd){
+	int old_flag = fcntl(fd,F_GETFD);
+	fcntl(fd,F_SETFD,old_flag|O_NONBLOCK);
+	return old_flag;
+}
+
+void epolladdfd(int epfd,int fd){
+	setnonblocking(fd);
+	epoll_event ev;
+	ev.data.fd = fd;	
+	ev.events = EPOLLIN | EPOLLET;
+	epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
+}
 
 int main(int argc, char** argv){
 	int ret = -1;
 	char buf[1024] = {'\0',};
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	errif(sockfd == -1, "fail to create socket fd!");
+
+	int epfd = epoll_create1(0);
 
 	struct sockaddr_in serv_addr;
 	bzero(&serv_addr, sizeof(serv_addr));
@@ -32,28 +49,34 @@ int main(int argc, char** argv){
 
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_len = sizeof(clnt_addr);
-	bzero(&clnt_addr, clnt_addr_len);
-
-	int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
-	errif(clnt_sockfd == -1, "fail to listen socket fd!");
-
-	printf("new client fd %d! IP: %s, Port: %d\n",clnt_sockfd,
+	epolladdfd(epfd, sockfd);
+	epoll_event events[1024];
+	bzero(&events, 1024);
+	while(1){
+		ret = epoll_wait(epfd, events, 1024, -1);	
+		for(int i = 0; i < ret; ++i){
+			if(events[i].data.fd == sockfd){
+				bzero(&clnt_addr, clnt_addr_len);
+				int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
+				epolladdfd(epfd, clnt_sockfd);
+				printf("new client fd %d! IP: %s, Port: %d\n",clnt_sockfd,
 												  inet_ntoa(clnt_addr.sin_addr), 
 												  ntohs(clnt_addr.sin_port));
-	while(1){
-		bzero(buf, sizeof(buf));
-		ret = recv(clnt_sockfd, buf, 1024, 0);		
-		if(ret > 0){
-			printf("%s\n", buf);
-		}else if(ret < 0){
-			errif(true, "socket receive message error!");
-			break;
+			}else if(events[i].events & EPOLLIN){
+				int clnt_sockfd = events[i].data.fd; 
+				bzero(buf, sizeof(buf));
+				ret = recv(clnt_sockfd, buf, 1024, 0);		
+				if(ret > 0){
+					printf("sockfd:%d, %s\n", clnt_sockfd,buf);
+				}else if(ret < 0){
+					errif(true, "socket receive message error!");
+					break;
+				}
+				ret = send(clnt_sockfd, buf, sizeof(buf), 0); 	
+				errif(ret == -1, "fail to send message!");	
+			} 	
 		}
-	    ret = send(clnt_sockfd, buf, ret, 0); 	
-		errif(ret == -1, "fail to send message!");
 	}
-	close(clnt_sockfd);
-	close(sockfd);
 	return 0;
 }
 
