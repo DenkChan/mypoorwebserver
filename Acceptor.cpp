87267e150da48b21c5c14e28180797caad1f99fc
Limerzzz <780039590@qq.com>
2022-04-10 16:58:51 +0800

    add connection class

--- Acceptor.cpp
+++ Acceptor.cpp
@@ -2,13 +2,14 @@
  * @Author: Limer
  * @Date: 2022-04-06 21:35:04
  * @LastEditors: Limer
- * @LastEditTime: 2022-04-06 21:42:31
+ * @LastEditTime: 2022-04-10 16:49:25
  * @Description:
  */
 #include "Acceptor.h"
 #include "Channel.h"
 #include "EventLoop.h"
 #include "InetAddress.h"
+#include "Server.h"
 #include "Socket.h"
 #include "util.h"
 
@@ -30,7 +31,16 @@ Acceptor::~Acceptor() {
     delete acptChl;
 }
 
-void Acceptor::acptConn() { newConnCallback(sock); }
+void Acceptor::acptConn() {
+    InetAddress* clnt_addr = new InetAddress();
+    Socket* clnt_sock = new Socket(sock->accept(clnt_addr));
+    printf("new client fd %d! IP: %s Port: %d\n", clnt_sock->get_fd(),
+           inet_ntoa(clnt_addr->_addr.sin_addr),
+           ntohs(clnt_addr->_addr.sin_port));
+    setnonblocking(clnt_sock->get_fd());
+    newConnCallback(clnt_sock);
+    delete clnt_addr;
+}
 
 void Acceptor::setNewConnCallback(std::function<void(Socket*)> _cb) {
     newConnCallback = _cb;
--- Server.cpp
+++ Server.cpp
@@ -2,7 +2,7 @@
  * @Author: Limer
  * @Date: 2022-04-03 15:53:26
  * @LastEditors: Limer
- * @LastEditTime: 2022-04-06 21:52:31
+ * @LastEditTime: 2022-04-10 16:57:00
  * @Description:
  */
 #include "Server.h"
@@ -13,6 +13,7 @@
 #include <functional>
 #include "Acceptor.h"
 #include "Channel.h"
+#include "Connection.h"
 #include "EventLoop.h"
 #include "Socket.h"
 #include "util.h"
@@ -28,37 +29,16 @@ Server::Server(Eventloop* lp) : _loop(lp) {
 
 Server::~Server() { delete acpt; }
 
-void Server::handleReadEvent(int fd) {
-    char buf[READ_BUFF_SIZE];
-    while (true) {
-        bzero(buf, READ_BUFF_SIZE);
-        ssize_t read_bytes = read(fd, buf, READ_BUFF_SIZE);
-        if (read_bytes > 0) {
-            printf("from: %d, %s\n", fd, buf);
-        } else if (read_bytes == -1 && errno == EAGAIN ||
-                   errno == EWOULDBLOCK) {
-            printf("finish reading\n");
-            break;
-        } else if (read_bytes == -1 && errno == EINTR) {
-            continue;
-        } else if (read_bytes == 0) {
-            printf("client close!\n");
-            close(fd);
-            break;
-        }
-    }
+void Server::newConnection(Socket* sock) {
+    Connection* conn = new Connection(_loop, sock);
+    std::function<void(Socket*)> cb =
+        std::bind(&Server::deleteConnection, this, std::placeholders::_1);
+    conn->setDeleteConnectionCallback(cb);
+    conns[sock->get_fd()] = conn;
 }
 
-void Server::newConnection(Socket* serv_sock) {
-    InetAddress* clnt_addr = new InetAddress();
-    Socket* clnt_sock = new Socket(serv_sock->accept(clnt_addr));
-    printf("new client fd %d! IP: %s Port: %d\n", clnt_sock->get_fd(),
-           inet_ntoa(clnt_addr->_addr.sin_addr),
-           ntohs(clnt_addr->_addr.sin_port));
-    setnonblocking(clnt_sock->get_fd());
-    Channel* clntChannel = new Channel(_loop, clnt_sock->get_fd());
-    std::function<void()> cb =
-        std::bind(&Server::handleReadEvent, this, clnt_sock->get_fd());
-    clntChannel->setCallback(cb);
-    clntChannel->enableReading();
+void Server::deleteConnection(Socket* sock) {
+    Connection* conn = conns[sock->get_fd()];
+    conns.erase(sock->get_fd());
+    delete conn;
 }
